2dcec18da52b5ecd3078fa2f83f441c40ff6d8d0
justin73 <mengjia@MacBook-Pro-de-Meng.local>
2017-02-10 19:19:22 -0500

    update nav effect

--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -4,13 +4,24 @@
       <router-link class="domain_section" to="/" exact>{{ $t("nav.domain") }}</router-link>
     </div><div class="nav_wrapper">
       <!-- <router-link class="nav_section" to="/" exact>{{ $t("nav.index") }}</router-link> -->
-      <router-link class="nav_section career" to="/career" exact>{{ $t("nav.projects") }}</router-link>
-      <router-link class="nav_section photo" to="/dashboard" exact>{{ $t("nav.photography") }}</router-link>
-      <router-link class="nav_section contact" to="/contact" exact>Contact</router-link>
+      <nav class="cl-effect-16" id="cl-effect-16">
+        <router-link class="nav_section career" to="/career" v-bind:data-hover="$t('nav.projects')" exact>{{ $t("nav.projects") }}</router-link>
+      </nav>
+      <nav class="cl-effect-16" id="cl-effect-16">
+        <router-link class="nav_section photo" to="/dashboard" v-bind:data-hover="$t('nav.photography')" exact>{{ $t("nav.photography") }}</router-link>
+      </nav>
+      <nav class="cl-effect-16" id="cl-effect-16">
+        <router-link class="nav_section contact" to="/contact" data-hover="Contact" exact>Contact</router-link>
+      </nav>
       <!-- <router-link class="nav_section" to="/events" exact>{{ $t("nav.projects") }}</router-link> -->
       <!-- <router-link class="nav_section" :to="{ path: 'services', query: { plan: 'private' }}" exact>{{ $t("nav.services") }}</router-link>
       <router-link class="nav_section" to="/clubs" exact>{{ $t("nav.clubs") }}</router-link>
       <router-link class="nav_section" to="/feeds" exact>{{ $t("nav.feeds") }}</router-link> -->
+<!--       <section class="color-11">
+        <nav class="cl-effect-16" id="cl-effect-16">
+          <a href="#cl-effect-16" data-hover="Languor">Languor</a>
+        </nav>
+      </section> -->
     </div>
   </div>
 </template>
