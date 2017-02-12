ac1d051a1d146403194c49a94c7e4ab216bb7894
Meng Jia <mengjia@MacBook-Pro-de-Meng.local>
2017-02-12 11:53:50 -0500

    job desc for nowinstore

--- src/templates/professional/nowinstore.vue
+++ src/templates/professional/nowinstore.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="cd-timeline" class="cd-container">
     <div class="cd-timeline-block" v-aos data-aos="fade-up-right">
-      <div class="cd-timeline-img cd-movie mcm">
-        <img src='../../assets/mcm.png'>
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content" >
@@ -13,115 +13,65 @@
     </div> <!-- cd-timeline-block -->
 
     <div class="cd-timeline-block" v-aos data-aos="fade-up-left">
-      <div class="cd-timeline-img cd-movie mcm">
-        <img src='../../assets/mcm.png'>
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content" >
         <h2>Starting from the basis</h2>
         <p>The first couple of days, I was trying to get my hands on the technologies used at NIS. Spending time to get to know the product, the tech stack. As a starter, I started working on templates using HTML, CSS, SASS, etc to render some static pages </p>
-        <span class="cd-date">June 2nd, 2014</span>
+        <span class="cd-date">June 4nd, 2014</span>
       </div> <!-- cd-timeline-content -->
     </div> <!-- cd-timeline-block -->
 
     <div class="cd-timeline-block" v-aos data-aos="fade-up-right">
-      <div class="cd-timeline-img cd-movie mcm">
-        <img src='../../assets/mcm.png'>
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content" >
-        <h2>Requirement Elicitation
+        <h2>First Major Push to Production
         </h2>
-        <p>Front-End work, mainly using Backbone.js, jQuery to build user interface</p>
-        <a href="#0" class="cd-read-more">Read more</a>
+        <p>I was assigned to create the new payment page based on the design provided, using bootstrap as the responsive framework.</p>
         <span class="cd-date">Mid June, 2014</span>
       </div> <!-- cd-timeline-content -->
     </div> <!-- cd-timeline-block -->
 
     <div class="cd-timeline-block" v-aos data-aos="fade-up-left">
-      <div class="cd-timeline-img cd-movie mcm">
-        <!-- <img src="img/cd-icon-movie.svg" alt="Movie"> -->
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content">
         <h2>Touching Backend Python logic</h2>
-        <p>Front-End work, mainly using Backbone.js, jQuery to build user interface</p>
-        <a href="#0" class="cd-read-more">Read more</a>
-        <span class="cd-date">Dec 3rd, 2014</span>
+        <p>After a couple of week working on the front-end tasks. I was given the tasks to fix some simple bugs on the backend, also used this opportunity to learn some practical knowlege on python. </p>
+        <span class="cd-date">July, 2014</span>
       </div> <!-- cd-timeline-content -->
     </div> <!-- cd-timeline-block -->
 
     <div class="cd-timeline-block" v-aos data-aos="fade-up-right">
-      <div class="cd-timeline-img cd-movie mcm">
-        <img src='../../assets/mcm.png'>
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content">
-        <h2>Integrated Payment System using Stripe.js for Campus Cloud </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-        <a href="#0" class="cd-read-more">Read more</a>
-        <span class="cd-date">July 2015</span>
-      </div> <!-- cd-timeline-content -->
-    </div> <!-- cd-timeline-block -->
-
-
-    <div class="cd-timeline-block" v-aos data-aos="fade-up-left">
-      <div class="cd-timeline-img cd-movie mcm">
-        <img src='../../assets/mcm.png'>
-      </div> <!-- cd-timeline-img -->
-
-      <div class="cd-timeline-content">
-        <h2>mcm New Website</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-        <a href="#0" class="cd-read-more">Read more</a>
-        <span class="cd-date">August 2016</span>
-      </div> <!-- cd-timeline-content -->
-    </div> <!-- cd-timeline-block -->
-
-    <div class="cd-timeline-block" v-aos data-aos="fade-up-right">
-      <div class="cd-timeline-img cd-movie mcm">
-        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
-      </div> <!-- cd-timeline-img -->
-
-      <div class="cd-timeline-content">
-        <h2>Event Attendance V1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-        <a href="#0" class="cd-read-more">Read more</a>
-        <span class="cd-date">Oct 2016</span>
+        <h2>Implement New Design for Catalog</h2>
+        <p>Updated the online catalog framework for the product and did integration with different eCommerce platforms.</p>
+        <span class="cd-date">Aug 2015</span>
       </div> <!-- cd-timeline-content -->
     </div> <!-- cd-timeline-block -->
 
     <div class="cd-timeline-block" v-aos data-aos="fade-up-left">
-      <div class="cd-timeline-img cd-movie mcm">
-        <!-- <img src="img/cd-icon-location.svg" alt="Location"> -->
+      <div class="cd-timeline-img cd-movie nis">
+        <img src='../../assets/nowinstore_logo.png'>
       </div> <!-- cd-timeline-img -->
 
       <div class="cd-timeline-content">
-        <h2>Event Attendance V2 & Routing Support for Campus Cloud</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
-        <a href="#0" class="cd-read-more">Read more</a>
-        <span class="cd-date">Dec 22nd, 2016</span>
+        <h2>This Internship Gave Me the Chance to Dive Into the Web Development World</h2>
+        <p>Before I started this internship, I have no practice on web development. But during the last 3 months, I realized that this is the wokr I want to do, especially on front-end development.</p>
+        <span class="cd-date">Aug 2015</span>
       </div> <!-- cd-timeline-content -->
     </div> <!-- cd-timeline-block -->
-
-    <div class="cd-timeline-block" v-aos data-aos="fade-up-right">
-      <div class="cd-timeline-img cd-movie mcm">
-      </div>
-      <div class="cd-timeline-content">
-        <h2>Service Assessment</h2>
-        <p>Led by me, the web team launced the support for Service Assessment as the last major feature for the winter loadmap. Campus Services now can be able to check-in students using the Campus Clouds and get instant rating & feedback</p>
-        <span class="cd-date">Jan 16th, 2017</span>
-      </div>
-    </div>
-    <div class="cd-timeline-block" v-aos data-aos="fade-up-left">
-      <div class="cd-timeline-img cd-movie mcm">
-      </div>
-      <div class="cd-timeline-content">
-        <h2>Campus Cloud 2.0</h2>
-        <p>Testing Vue.js as the front-end framework for the Campus Cloud 2.0. Working with the product designer for the new design</p>
-        <span class="cd-date">Til Now</span>
-      </div>
-    </div>
   </section>
 </template>
 <script>
